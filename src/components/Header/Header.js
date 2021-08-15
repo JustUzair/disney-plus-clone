b685200fdb1613b07700f72f419c9de70db7d300
Uzair <uzairhajra76330@gmail.com>
2021-08-15 12:43:54 +0530

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../../images/logo.svg";
+import homeIcon from "../../images/home-icon.svg";
+import searchIcon from "../../images/search-icon.svg";
+import watchlistIcon from "../../images/watchlist-icon.svg";
+import originalIcon from "../../images/original-icon.svg";
+import movieIcon from "../../images/movie-icon.svg";
+import seriesIcon from "../../images/series-icon.svg";
+import avatar from "../../images/avatar.png";
+function Header() {
+  return (
+    <NavWrapper>
+      <Logo src={logo} alt="disney+ hotstar" />
+      <NavMenu>
+        <a>
+          <img src={homeIcon} alt="home-icon" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src={searchIcon} alt="search-icon" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src={watchlistIcon} alt="watchlist-icon" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src={originalIcon} alt="originals-icon" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src={movieIcon} alt="movies-icon" />
+          <span>MOVIE</span>
+        </a>
+        <a>
+          <img src={seriesIcon} alt="series-icon" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src={avatar}></UserImg>
+    </NavWrapper>
+  );
+}
+const NavWrapper = styled.nav`
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+
+  align-items: center;
+  padding: 0 36px;
+`;
+const Logo = styled.img`
+  width: 80px;
+  cursor: pointer;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  padding: 0 20px;
+  flex: 1;
+  margin-left: 20px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+
+      position: relative;
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        right: 0;
+        left: 0;
+        bottom: -6px;
+        transition: all 0.25s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+        transform-origin: left center;
+        opacity: 0;
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  height: 48px;
+  width: 48px;
+  border-radius: 50%;
+  justify-self: right;
+  cursor: pointer;
+`;
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Header from "../Header/Header";
+function Home() {
+  return <div>HOme</div>;
+}
+
+export default Home;
